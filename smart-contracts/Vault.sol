4a8718853a08a2112ee8c70c52668d6339c7e084
cbecerrc <52862131+cbecerrc@users.noreply.github.com>
2024-05-23 15:04:24 +0700

    style: lint and format entire codebase in Vault.sol

--- smart-contracts/Vault.sol
+++ smart-contracts/Vault.sol
@@ -39,3 +39,4 @@ contract EnterpriseYieldVault is ReentrancyGuard, Ownable {
 // Optimized logic batch 3748
 // Optimized logic batch 2222
 // Optimized logic batch 7041
+// Optimized logic batch 5841
