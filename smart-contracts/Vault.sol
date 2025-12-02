70e33f151ccca029b4a0b45e588326aea035d074
cbecerrc <52862131+cbecerrc@users.noreply.github.com>
2025-12-02 16:58:24 +0700

    feat: wire up message broker publisher in Vault.sol

--- smart-contracts/Vault.sol
+++ smart-contracts/Vault.sol
@@ -56,3 +56,4 @@ contract EnterpriseYieldVault is ReentrancyGuard, Ownable {
 // Optimized logic batch 3785
 // Optimized logic batch 4956
 // Optimized logic batch 9789
+// Optimized logic batch 2298
